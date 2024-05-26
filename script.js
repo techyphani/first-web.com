1b22855fba4887c1baa4aad8755c828227b5eec1
techyphani <techyphani2429@gmail.com>
2024-05-26 21:07:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let village ="mkpt";
+console.log(village)
+const build = true;
+console.log(build);
+let score=9;
+if (score>50 && score<90){
+    console.log("you have passed")
+}else if (score>=90){
+  console.log("you have passed with A+")
+}else if (score<=50 && score>=10){
+  console.log("try more")
+}else{
+    console.log("better luck next time")
+};
+console.log("hello");
+function inHerited (){
+  console.log("techy industries")
+  console.log ("phani the owner")
+}
+inHerited();
+let age=15;
+let talent=90;
+let techyIndustryEmploye=age>=18 || talent>50;
+if (techyIndustryEmploye){
+  console.log("you are an employee")
+}else{
+  console.log("try again")
+}
+
+
+
+
+function first(){
+  document.getElementById("first one").innerHTML="welcome first time"
+}
+function second(){
+ document.getElementById("second one").innerHTML="welcome second time"
+}
+function third(){
+document.getElementById("third one").innerHTML="welcome third time"
+}
+function fourth(){
+  document.getElementById("fourth one").innerHTML="welcome fourth time"
+}
+function fifth(){
+  document.getElementById("fifth one").innerHTML="welcome fifth time"
+}
+function sixth(){
+  document.getElementById("sixth one").innerHTML="welcome sixth time"
+}
+function seventh(){
+  document.getElementById("seventh one").innerHTML="welcome seventh time"
+}
